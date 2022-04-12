e405ecba1f4b9483291fe6ad65c8d3946ff52809
elsamaryissac <elsajm15@gmail.com>
2022-04-12 12:09:16 +0530

    Initial Commit.

---
+++ src/com/helloworld/Data.java
@@ -0,0 +1,19 @@
+package com.helloworld;
+
+/**
+ * This class is used to handle data.
+ * @author LEGEND
+ *
+ */
+public class Data {
+
+	/**
+	 * This method is used to print a String.
+	 * @param value It is used to get a value.
+	 * @param index It is used to get an integer value.
+	 */
+	public static void print(String value, Integer index) {
+		System.out.println(value + index);
+	}
+	
+}
---
+++ src/com/helloworld/DataHandler.java
@@ -0,0 +1,9 @@
+package com.helloworld;
+
+public class DataHandler {
+	public static void main(String args[]) {
+		Data.print("Hello World", 5);
+		
+	}
+
+}
